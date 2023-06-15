3dcca59e6d3b1cf95126360cb73ae25fd9b989cd
nahidHassan405 <infosharemail405@gmail.com>
2023-06-15 11:59:18 +0600

    kolas

--- starter/script.js
+++ starter/script.js
@@ -5,3 +5,36 @@ if (birthYear <= 2000) {
 } else {
   console.log("Born in 21 century");
 }
+
+const favourite = 14;
+
+if (favourite > 0) {
+  console.log("great number");
+} else {
+  console.log("try something new");
+}
+
+const a = false;
+if (!a) {
+  console.log("its done");
+}
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+const isTired = false;
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Asif is able to drive!");
+} else {
+  console.log("someone else should drive...");
+}
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins0) {
+  console.log("Koalas win the trophy");
+} else {
+  console.log("Both win the trophy");
+}
