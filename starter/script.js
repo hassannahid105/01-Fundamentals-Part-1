69dca6e5d4048ff9132fe358ccba753b88abfe16
nahidHassan405 <infosharemail405@gmail.com>
2023-06-15 12:35:59 +0600

    day

--- starter/script.js
+++ starter/script.js
@@ -38,3 +38,30 @@ if (scoreDolphins > scoreKoalas) {
 } else {
   console.log("Both win the trophy");
 }
+
+// const day = 'sat'
+// switch(day){
+//   case 'mon':
+//   console.log('Plane Course structure');
+//   console.log('Go to coding meetup');
+//   break;
+//   case "tues":
+
+//     break;
+//     case "t"
+// }
+const day = "thus";
+if (day === "mon") {
+  console.log("Plane Course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tues") {
+  console.log("Prepare theory video");
+} else if (day === "wed" || day === "thus") {
+  console.log("write code example");
+} else if (day === "fri") {
+  console.log("Record video");
+} else if (day === "sat" || day === "sun") {
+  console.log("Enjoy the weekend");
+} else {
+  console.log("Not a valid day");
+}
