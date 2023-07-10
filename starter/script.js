8860b59fdf1081f9f489d6b9c0e7e234b4a766bd
nahidHassan405 <infosharemail405@gmail.com>
2023-07-10 11:47:25 +0600

    not update

--- starter/script.js
+++ starter/script.js
@@ -65,3 +65,12 @@ if (day === "mon") {
 } else {
   console.log("Not a valid day");
 }
+
+const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
